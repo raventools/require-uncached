1bb309cdfd5e7f9a8339e66954d3a1a7e26b703d
Jon Zumbrun <jzumbrun@gmail.com>
2014-10-20 14:22:39 -0500

    A simple delete cache was not working. Remove cached paths too.

--- index.js
+++ index.js
@@ -3,6 +3,51 @@ var path = require('path');
 var resolveFrom = require('resolve-from');
 var callerPath = require('caller-path');
 
+/**
+ * Removes a module from the cache
+ */
+require.uncache = function (moduleName) {
+	// Run over the cache looking for the files
+	// loaded by the specified module name
+	require.searchCache(moduleName, function (mod) {
+		delete require.cache[mod.id];
+	});
+
+	// Remove cached paths to the module.
+	// Thanks to @bentael for pointing this out.
+	Object.keys(module.constructor._pathCache).forEach(function(cacheKey) {
+		if (cacheKey.indexOf(moduleName)>0) {
+			delete module.constructor._pathCache[cacheKey];
+		}
+	});
+};
+
+/**
+ * Runs over the cache to search for all the cached
+ * files
+ */
+require.searchCache = function (moduleName, callback) {
+	// Resolve the module identified by the specified name
+	var mod = require.resolve(moduleName);
+
+	// Check if the module has been resolved and found within
+	// the cache
+	if (mod && ((mod = require.cache[mod]) !== undefined)) {
+		// Recursively go over the results
+		(function run(mod) {
+			// Go over each of the module's children and
+			// run over it
+			mod.children.forEach(function (child) {
+				run(child);
+			});
+
+			// Call the specified callback providing the
+			// found module
+			callback(mod);
+		})(mod);
+	}
+};
+
 module.exports = function (moduleId) {
 	if (typeof moduleId !== 'string') {
 		throw new TypeError('Expected a string');
@@ -10,7 +55,7 @@ module.exports = function (moduleId) {
 
 	var filePath = resolveFrom(path.dirname(callerPath()), moduleId);
 	var tmp = require.cache[filePath];
-	delete require.cache[filePath];
+	require.uncache(filePath);
 	var ret = require(filePath);
 	require.cache[filePath] = tmp;
 
